1b5a0f6a88eeb51e9a0ca2ec785a8bd4069f10c2
Pavel Martysiuk <PMartysi@ITS.JNJ.com>
2022-03-13 18:09:06 +0300

    delete imprts

--- sako/main/admin.py
+++ sako/main/admin.py
@@ -3,17 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from main.models import ProductLogo, Product, Category, Contacts, Partner, About, Manufacturer
 
-from django.contrib.admin.options import (
-    HORIZONTAL, VERTICAL, ModelAdmin, StackedInline, TabularInline,
-)
-
-# def ProductAdmin(ModelAdmin):
-#     def formfield_for_dbfield(self, db_field, request, **kwargs):
-#         if db_field.name == 'description':
-#             kwargs['strip'] = False
-#         return super().formfield_for_dbfield(db_field, request, **kwargs)
-#
-
 admin.site.register(ProductLogo)
 admin.site.register(Product, )
 admin.site.register(Category)
